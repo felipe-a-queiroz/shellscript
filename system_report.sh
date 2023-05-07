68ebbb3064830cd45edd97a3bf7b90dfed26b020
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2023-05-07 11:56:25 -0300

    Args and System report

---
+++ system_report.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "==============================================="
+echo "Relatório da Máquina: $(hostname)"
+echo "Data/Hora: $(date)"
+echo ""
+echo "Máquina Ativa desde: $(uptime)"
---
+++ two_input_args.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read -p 'Enter the first number: ' NUMBER_1
+read -p 'Enter the second number: ' NUMBER_2
+echo "The result is $((NUMBER_1 + NUMBER_2))"
