a62fa315c89560a1f48c38ab6150d54026d3952d
Felipe <97626017+felipe-a-queiroz@users.noreply.github.com>
2023-05-08 16:58:09 -0300

    Update system_report.sh

--- system_report.sh
+++ system_report.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+KERNEL=$(uname -r)
+HOSTNAME=$(hostname)
+CPUNO=$(cat /proc/cpuinfo | grep 'model name' | wc -l)
+CPUMODEL=$(cat /proc/cpuinfo | grep 'model name' | head -n1 | cut -c14-)
+MEMTOTAL=$(cat /proc/meminfo | grep 'MemTotal' | cut -d: -f2)
+FILESYS=$(df -h)
+UPTIME=$(uptime)
 echo "==============================================="
-echo "Relatório da Máquina: $(hostname)"
+echo "Relatório da Máquina: $HOSTNAME"
 echo "Data/Hora: $(date)"
 echo ""
-echo "Máquina Ativa desde: $(uptime)"
+echo "Máquina Ativa desde: $UPTIME"
+echo ""
+echo "Versão do Kernel: $KERNEL"
+echo ""
+echo "CPUs:"
+echo "Quantidade de CPUs/Core: $CPUNO"
+echo "Modelo da CPU: $CPUMODEL"
+echo ""
+echo "Memória Total: $MEMTOTAL"
+echo ""
+echo "Partições:"
+echo "$FILESYS"
+echo "==============================================="
